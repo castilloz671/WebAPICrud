8a339b457e9e73f84beb44a32f37c981ffe4893a
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2024-05-28 21:10:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPICrud.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbcrudContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigins", app =>
+    {
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.UseCors("AllowOrigins");
+
+app.MapControllers();
+
+app.Run();
